110035a320b3f95d590a35b71ba57fd5a5abe365
mjgumbs <mjgumbs.200@gmail.com>
2015-11-03 16:07:25 -0400

    waereaf

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Gesmew::Core::Engine.add_routes do
     get '/checkout/registration' => 'checkout#registration', :as => :checkout_registration
     put '/checkout/registration' => 'checkout#update_registration', :as => :update_checkout_registration
 
-    # resource :account, :controller => 'users'
+    resource :account, :controller => 'users'
 
     namespace :admin do
       devise_for :gesmew_user,
